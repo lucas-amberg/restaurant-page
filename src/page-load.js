019e0c70706ddc111a215b7ca4d3f0018720f823
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-08-08 23:23:36 -0700

    Add more cards to homepage Add the hero, address, and hours cards to the homepage

--- src/page-load.js
+++ src/page-load.js
@@ -1,13 +1,17 @@
 import burgerImage from "./images/burger-image.jpg"
 
 function loadPage() {
+
+    //Grab content div
     const contentContainer = document.getElementById("content");
     const items = [];
 
+    //Create heading section
     const heading = document.createElement("div");
     heading.classList.add("heading");
     items.push(heading);
 
+    //Create heading text
     const homeHeading = document.createElement("h1");
     homeHeading.textContent = "AMBURGERS";
     homeHeading.classList.add("home-heading-text");
@@ -18,14 +22,62 @@ function loadPage() {
     homeHeadingSubText.classList.add("home-heading-subtext");
     heading.appendChild(homeHeadingSubText);
 
+    //Create body section
+    const mainBody = document.createElement("div");
+    mainBody.classList.add("main-body-container");
+    items.push(mainBody);
+
+    //Create burger hero (1st hero)
+    const burgerHero = document.createElement("div");
+    burgerHero.classList.add("burger-hero-container");
+    const burgerHeroText = document.createElement("div");
+    burgerHeroText.classList.add("burger-hero-text");
+    burgerHeroText.textContent = "We have the worlds number one bussiest buss, and we can prove it.";
+
+    //Add burger image to hero
     const homeBurgerImage = new Image();
     homeBurgerImage.src = burgerImage;
     homeBurgerImage.classList.add("burger-image");
-    items.push(homeBurgerImage);
+    mainBody.appendChild(burgerHero);
+    burgerHero.appendChild(homeBurgerImage);
+    burgerHero.appendChild(burgerHeroText);
+
+    //Create address section
+    const addressSection = document.createElement("div");
+    mainBody.appendChild(addressSection);
+
+    const addressHeader = document.createElement("h2");
+    addressSection.appendChild(addressHeader);
+    addressHeader.textContent = "Come visit us!"
+
+    const addressText = document.createElement("div");
+    addressSection.appendChild(addressText);
+    addressText.innerHTML = "945 Oakmont Street<br>Los Angeles, CA<br>90009";
+
+    addressSection.classList.add("address-section");
+
+    //Create hours section
+    const hoursSection = document.createElement("div");
+    mainBody.appendChild(hoursSection);
+    hoursSection.classList.add("hours-section");
+
+    const hoursHeading = document.createElement("h2");
+    const hoursText = document.createElement("div");
+    const hoursFooter = document.createElement("h3");
+
+    hoursHeading.textContent = "We are open";
+    hoursText.textContent = "24/7/365";
+    hoursFooter.textContent = "Because you just couldn't get enough.";
+
+    hoursSection.appendChild(hoursHeading);
+    hoursSection.appendChild(hoursText);
+    hoursSection.appendChild(hoursFooter);
 
     for (let i = 0; i < items.length; i++) {
         contentContainer.appendChild(items[i]);
     }
 }
+
+
 
 export default loadPage;
